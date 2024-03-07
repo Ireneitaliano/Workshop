130ad9937a7751dbed01c3f4458862d40281bf04
Ireneitaliano <162457414+Ireneitaliano@users.noreply.github.com>
2024-03-07 16:48:25 +0100

    7mar

---
+++ src/components/Card_festival.svelte
@@ -0,0 +1,46 @@
+<script>
+	export let Titolo;
+	export let Descrizione;
+	export let Bottone;
+</script>
+
+<div class="card_festival">
+	<p class="titolo">{Titolo}</p>
+	<p class="descrizione">{Descrizione}</p>
+	<div class="card_bottone">
+		<p class="bottone">{Bottone}</p>
+	</div>
+</div>
+
+<style>
+	.card_festival {
+		background-color: #000;
+		width: fit-content;
+		border-radius: 20px;
+		padding: 10px;
+		color: #14ff00;
+		font-family: sans-serif;
+	}
+
+	.titolo {
+		text-align: center;
+		font-size: 32px;
+	}
+
+	.descrizione {
+		text-align: center;
+		font-size: 9px;
+	}
+
+	.card_bottone {
+		background-color: #14ff00;
+		padding: 10px;
+		border-radius: 20px;
+	}
+
+	.bottone {
+		text-align: center;
+		color: white;
+		font-size: 12px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
-	import { get_collection } from '$cms';
+	import Card_festival from '$components/Card_festival.svelte';
+
+	let Cards = [
+		{
+			Titolo: 'TICKET',
+			Descrizione: 'Choose the best ticket based on your needs',
+			Bottone: 'Get them'
+		},
+		{ Titolo: 'HOTEL', Descrizione: 'Choose the best hotel', Bottone: 'Book now' },
+		{
+			Titolo: 'TRANSPORT',
+			Descrizione: 'Discover all the possibilities to reach us',
+			Bottone: 'Come now'
+		},
+		{
+			Titolo: 'ACTIVITIES',
+			Descrizione: 'Make your experience even more enjoyable',
+			Bottone: 'Enjoy'
+		}
+	];
 </script>
 
-Home page
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+
+{#each Cards as Card}
+	<Card_festival Titolo={Card.Titolo} Descrizione={Card.Descrizione} Bottone={Card.Bottone} />
+{/each}
